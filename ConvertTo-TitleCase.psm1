86778562afb8833344ccc117739da3ba695df365
Anthony J. Celaya <anthony.celaya@scccd.edu>
2018-11-06 20:00:04 -0800

    initial commit

---
+++ ConvertTo-TitleCase.psm1
@@ -0,0 +1,19 @@
+
+<#
+NAME............: ConvertTo-TitleCase.psm1
+AUTHOR..........: Anthony J. Celaya
+DATE............: 05-15-2017
+DESCRIPTION.....: Convert string to Title Case
+NOTES...........: 
+#>
+
+Function ConvertTo-TitleCase(){
+[CmdletBinding()]
+Param(
+    [string]$string
+)
+
+(Get-Culture).TextInfo.ToTitleCase($string.ToLower())
+
+
+}#end Function ConvertTo-TitleCase{}
